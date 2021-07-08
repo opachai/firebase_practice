0b81e33a4a99edb85a6921b7ddfe403e022e6a0a
opachai <f.kaba929@gmail.com>
2021-07-08 23:48:00 +0900

    getting data

--- public/js/org-firestore.js
+++ public/js/org-firestore.js
@@ -1,5 +1,58 @@
 var db = firebase.firestore();
 var db_user = db.collection('users');
+var doc = db_user.doc('sample');
+
+/////////////////////////////////
+///////// データの読み込み /////////
+/////////////////////////////////
+
+// get()がdocumentの取得
+function getDocument(path) {
+    console.log('単一ドキュメントの取得');
+    doc.get().then((doc) => {
+        console.log(doc.data());
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+};
+
+function getDocuments(path) {
+    console.log('複数ドキュメントの取得');
+    db_user.get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+        });
+    });
+};
+
+function filterDocuments(path,cond,comp,val) {
+    console.log('複数ドキュメントから絞り込んで取得');
+    db_user.where(cond,comp,val).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+        });
+    }).catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+};
+
+getDocument(doc);
+
+getDocuments(db_user);
+
+filterDocuments(db_user,"age", "==", "22",);
+
+
+
+
 
 // データの書き込み
 
@@ -9,7 +62,7 @@ const data = {
   age: '55',
   sample: "new data"
 };
-db_user.doc('sample').set(data);
+// db_user.doc('sample').set(data);
 
 
 // documentを指定せずにコレクションに追加（document idは自動採番）：add
@@ -21,14 +74,3 @@ const data2 = {
 // db_user.add(data2);
 
 
-// データの読み込み
-db_user.doc('sample').get().then((doc) => {
-    if (doc.exists) {
-        console.log("Document data:", doc.data());
-    } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-    }
-}).catch((error) => {
-    console.log("Error getting document:", error);
-});
